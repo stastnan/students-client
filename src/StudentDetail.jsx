dd01302468bd582f0f4bb81313795e8a97d1cc8c
stastnan <nik.stastna@gmail.com>
2023-11-25 12:02:19 +0100

    Added basic styles and students list + detail

--- src/StudentDetail.jsx
+++ src/StudentDetail.jsx
@@ -1,7 +1,49 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export const StudentDetail = () => {
+  const [student, setStudent] = useState("");
   const { id } = useParams();
 
-  return <p>Tady bude detail studenta č. {id}.</p>;
+  useEffect(() => {
+    fetch(`http://localhost:8080/students/${id}`)
+      .then((response) => response.json())
+      .then((body) => {
+        setStudent(body);
+      });
+  }, [id]);
+
+  return (
+    <>
+      <h1>Student detail</h1>
+      <table className="table table-light table-bordered">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <td>
+              {student.firstName} {student.lastName}
+            </td>
+          </tr>
+          <tr>
+            <th>Gender</th>
+            <td>{student.gender}</td>
+          </tr>
+          <tr>
+            <th>House</th>
+            <td>{student.house}</td>
+          </tr>
+          <tr>
+            <th>Year</th>
+            <td>{student.year}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div style={{ display: "flex", gap: 20 }}>
+        <Link to="/">Back to student list</Link>
+        <Link to={`/students/${id}/edit`}>
+          Edit {student.firstName} {student.lastName}
+        </Link>
+      </div>
+    </>
+  );
 };
--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const StudentList = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/students")
+      .then((response) => response.json())
+      .then((body) => {
+        setStudents(body);
+      });
+  }, []);
+
+  const handleStudentDelete = (id) => {
+    const filteredStudents = students.filter((student) => student.id !== id);
+    setStudents(filteredStudents);
+  };
+
   return (
     <>
-      <p>Tady bude seznam studentů.</p>
-      <p>
-        <Link to="/students/create">Vytvoření studenta.</Link>
-      </p>
-      <p>
-        <Link to="/students/1">Detail studenta č. 1.</Link>
-      </p>
+      <h1>List of students</h1>
+      {students.length > 0 ? (
+        <table className="table table-light table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>House</th>
+              <th>Year</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student) => (
+              <tr key={student.id}>
+                <td>
+                  <Link to={`/students/${student.id}`}>
+                    {student.firstName} {student.lastName}
+                  </Link>
+                </td>
+                <td>{student.gender}</td>
+                <td>{student.house}</td>
+                <td>{student.year}</td>
+                <td>
+                  <Link to={`/students/${student.id}/edit`}> Edit</Link>
+                  <button
+                    type="button"
+                    className="btn btn-danger student-delete"
+                    onClick={() => handleStudentDelete(student.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
+      <Link to="/students/create">Create new student</Link>
     </>
   );
 };
